d7da6e683b9e032a71e34c4f8290f6f52f92db3f
Dylan Fantauzzo <dylan.fantauzzo7@proton.me>
2025-12-16 19:22:10 +0100

    initial commit

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,23 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("{secret_number}");
+    println!("Guessing game");
+    loop {
+        println!("Guess a number between 1 - 100: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        println!("You guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You won!"),
+        }
+    }
+}
